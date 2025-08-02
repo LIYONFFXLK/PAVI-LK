90518233400fa2fcc32d8977a14e5c6dfa4af82b
LIYONFFXLK <ffdenu58@gmail.com>
2025-08-02 08:38:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/LIYONFFXLK/PAVI-LK/blob/main/images/1754098410527.png",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 PAVI-MD Is Alive Now😍*",
+BOT_OWNER: '94720630290',  // Replace with the owner's phone number
 
 
 

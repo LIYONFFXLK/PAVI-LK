3a2befd8abb402daf79b94f516a42a3ba0cae442
LIYONFFXLK <ffdenu58@gmail.com>
2025-08-02 09:56:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SJ1BzZ5L#Pok8zqC7DNogupgn7Nd6fHvd1wIJc-zIW6oIj2OYvWQ",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/LIYONFFXLK/PAVI-LK/blob/main/images/1754098410527.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 PAVI-MD Is Alive Now😍*",
 BOT_OWNER: '94720630290',  // Replace with the owner's phone number

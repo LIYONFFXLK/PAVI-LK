1113248d5d6b628476c2ebf823ef7944a35ac294
LIYONFFXLK <ffdenu58@gmail.com>
2025-08-02 16:01:29 +0530

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -5,6 +5,7 @@ const { ytmp3 } = require("@vreden/youtube_scraper");
 cmd(
   {
     pattern: "song",
+    alias: ["song", "downloadsong"],
     react: "🎶",
     desc: "Download Song",
     category: "download",
